87ea526d5f099690eea3a1ec1c8d8a257bf89202
Rustemen <rmenadziev6@gmail.com>
2021-03-04 08:00:03 +0300

    5 commit

---
+++ Practice/02/C++/ConsoleApplication1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a1 = 3;
+    int a2 = 3.14;
+
+    double b1 = 3;
+    double b2 = 3.14;
+
+    cout << a1 << endl;
+    cout << a2 << endl;
+    // переменные типа int хранят целые значения чисел
+    cout << b1 << endl;
+    cout << b2 << endl;
+    // переменные типа double могут принимать более точные значения
+   
+    return 0;
+}
